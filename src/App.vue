9293cc930404fffb4d7831e26c0e8edc566b2e72
Alejandro Leal <aleaberm12@gmail.com>
2021-12-08 23:25:18 +0100

    fixes

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,11 @@ const { repositories } = storeToRefs(main);
 </script>
 
 <template>
-  <div class="bg-gray-800 h-screen w-screen font-sans">
-    <div class="flex flex-col items-center">
-      <div class="pt-6 pb-8">
-        <a href="https://github.com/AstroCorp" target="_blank" class="bg-logo block h-20 w-20 rounded-full overflow-hidden p-2.5">
-          <img src="/assets/logo.png" alt="logo" class="-mt-0.5" />
-        </a>
-      </div>
+  <div class="bg-gray-800 h-full w-full font-sans">
+    <div class="flex flex-col items-center py-6">
+      <a href="https://github.com/AstroCorp" target="_blank" class="bg-logo block h-20 w-20 mb-6 rounded-full overflow-hidden p-2.5">
+        <img src="/assets/logo.png" alt="logo" class="-mt-0.5" />
+      </a>
       
       <div class="text-center">
         <Card v-for="repo in repositories" :key="repo.title" :repository="repo" />
